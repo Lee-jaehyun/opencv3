46479da7fcd2118bcba125bd2a305773bbfdec90
Lee-jaehyun <62226548+Lee-jaehyun@users.noreply.github.com>
2022-02-07 09:38:55 +0900

    내용추가~!

--- camshift.py
+++ camshift.py
@@ -1,6 +1,10 @@
 import cv2
 import sys
+import numpy as np
+import argparse
 
+
+'''
 cap = cv2.VideoCapture('/Users/jaehyuni/Downloads/IMG_1485.MOV')
 
 if not cap.isOpened():
@@ -53,8 +57,46 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+'''
 
-
+parser = argparse.ArgumentParser(description='This sample demonstrates the camshift algorithm. \
+                                              The example file can be downloaded from: \
+                                              https://www.bogotobogo.com/python/OpenCV_Python/images/mean_shift_tracking/slow_traffic_small.mp4')
+parser.add_argument('image', type=str, help='path to image file')
+args = parser.parse_args()
+cap = cv2.VideoCapture(args.image)
+# take first frame of the video
+ret,frame = cap.read()
+# setup initial location of window
+x, y, w, h = 300, 200, 100, 50 # simply hardcoded the values
+track_window = (x, y, w, h)
+# set up the ROI for tracking
+roi = frame[y:y+h, x:x+w]
+hsv_roi =  cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
+roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
+cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
+# Setup the termination criteria, either 10 iteration or move by atleast 1 pt
+term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
+while(1):
+    ret, frame = cap.read()
+    if ret == True:
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
+        # apply camshift to get the new location
+        ret, track_window = cv2.CamShift(dst, track_window, term_crit)
+        # Draw it on image
+        pts = cv2.boxPoints(ret)
+        pts = np.int0(pts)
+        img2 = cv2.polylines(frame,[pts],True, 255,2)
+        cv2.imshow('img2',img2)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
+    else:
+        break
 
 
 ## 출처 : http://www.gisdeveloper.co.kr/?p=6838
+
+## 출처 : https://docs.opencv.org/4.3.0/d7/d00/tutorial_meanshift.html
